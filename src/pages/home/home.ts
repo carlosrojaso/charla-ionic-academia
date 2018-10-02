f825264aab6790a7b533017f0b2429cd8eac80ec
Carlos Rojas <Carlos.Y.Rojas.-ND@disney.com>
2018-10-02 14:22:54 -0500

    forgot + logout

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {AuthProvider} from '../../providers/auth/auth';
+import {LoginPage} from '../login/login';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  //private rootPage: any = StartPage;
+  constructor( public nav: NavController, public authData: AuthProvider) {
+  this.authData = authData;
   }
 
+ logOut(){
+  this.authData.logoutUser().then(() => {
+    this.nav.setRoot(LoginPage);
+  });
+}
 }
